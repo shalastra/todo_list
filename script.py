535df1c7b392046820b2be1bd76981b738f44193
Szymon Halastra <szymon.halastra@gmail.com>
2017-11-30 13:10:58 +0100

    Modify script

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 #!flask/bin/python
-import argparse
+import argparse, sys, os
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+tasks = {}
 
 parser=argparse.ArgumentParser(
     description='''Simple RESTful server ofr handling basic HTTP operations, serves as a todolist application. 
@@ -9,3 +16,12 @@ to test getting one element, run:\n
 curl -i http://localhost:5000/todo/tasks/<id> ''',
     epilog="""""")
 args=parser.parse_args()
+
+def rest_server(port):
+    print("test")
+
+def main(argv):
+    rest_server(8080)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
