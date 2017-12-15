266087d03f8b17e2642224c9aca3865175fb287b
Szymon Halastra <szymon.halastra@gmail.com>
2017-12-15 11:04:34 +0100

    Add absolute path of directory

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import os
 import sys
 
 from restserver import rest_server
@@ -8,8 +9,10 @@ from restserver import rest_server
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+# gets absolute path to scripts directory
+path= os.path.dirname(os.path.abspath(__file__))
 # opens help.txt file where help text is stored, accessed by -h or --help
-with open('help.txt') as f:
+with open(path+'/help.txt') as f:
     content = f.readlines()
 
 parser = argparse.ArgumentParser(
